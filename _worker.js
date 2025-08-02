b6051fde320d0cf7b170a8119a01339f88d92ffc
Muhammad Amzar <112941665+muhammadamzar@users.noreply.github.com>
2025-08-03 00:48:47 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'Masukan Host kamu bila tidak work harus pointing proxied'
+      url.hostname = 'Masukan Host kamu bila tidak work harus pointing proxied (amzar-vpn.amzarserver.my.id)'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
